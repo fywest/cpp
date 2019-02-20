4f904beda57d71865ccb86872c8d2e542488cf5b
fywest <fywest2109@hotmail.com>
2019-02-20 23:07:28 +0200

    3-3 and 3-4 added

---
+++ 3-4.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    //double f;
+    //int n;
+    //freopen("t.txt","r",stdin);
+    //cin >> f >> n;
+    //cout << f << "," << n << endl;
+
+    int x;
+    char buf[100];
+    cin >> x;
+    cin.getline(buf,90);
+    cout << buf << endl;
+    
+    return 0;
+
+}
